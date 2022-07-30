cf184238c35152cd99d717ad3d914cf447cb3f94
myb12 <mohammadyasinbappy@outlook.com>
2022-07-30 12:47:35 +0600

    Header component is build

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import './styles/main.scss';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import Header from './components/shared/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
+
+/* 
+https://i.ibb.co/4JCtvvW/side-2.webp
+https://i.ibb.co/0QThWHM/side-3.webp
+https://i.ibb.co/jDKHv2R/top-1.webp
+https://i.ibb.co/RBWD3Jb/top-2.webp
+https://i.ibb.co/xqkvzS7/top-3.webp
+https://i.ibb.co/zN721VV/side-1.webp
+*/
