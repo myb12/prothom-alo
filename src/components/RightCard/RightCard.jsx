750f94437094cb7f39ca4b05fab87a2652ed1663
myb12 <mohammadyasinbappy@outlook.com>
2022-07-31 01:57:19 +0600

    Some data related issues are resolved as well as readme file is edited

--- src/components/RightCard/RightCard.jsx
+++ src/components/RightCard/RightCard.jsx
@@ -7,7 +7,7 @@ const RightCard = ({ data }) => {
         <div className='right-card' onClick={() => navigateToDetail('selected', 1)}>
             <div className='right-card-body'>
                 <h3>
-                    <span>{data?.subheadline} / </span> প্রথম বিশ্বযুদ্ধের ঝানু বৈমানিক
+                    <span>{data?.subheadline} / </span> {data?.headline}
                 </h3>
                 <img src={data?.coverPhoto} alt="" />
             </div>
